2d51b791b9e152653ed49b67150087b547689150
zorluozan <61868614+zorluozan@users.noreply.github.com>
2021-08-18 18:13:43 +0300

    Add expense summary to dashboard

---
+++ src/tests/selectors/expenses-total.test.js
@@ -0,0 +1,19 @@
+import React from "react";
+import moment from "moment";
+import selectExpensesTotal from "../../selectors/expenses-total";
+import expenses from "../fixtures/expenses";
+
+test("should return 0 if no expenses", () => {
+  const res = selectExpensesTotal([]);
+  expect(res).toBe(0);
+});
+
+test("should correctly add up a single expense", () => {
+  const res = selectExpensesTotal([expenses[0]]);
+  expect(res).toBe(195);
+});
+
+test("should correctly add up multiple expense", () => {
+  const res = selectExpensesTotal(expenses);
+  expect(res).toBe(114195);
+});
